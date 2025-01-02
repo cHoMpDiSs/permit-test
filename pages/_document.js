a71919198192056b75b2dd8c6a9de21fb89e5e79
Jordon Marchesano <jordonomarchesano@gmail.com>
2025-01-01 21:56:54 -0800

    meta

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,15 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+       <Head>
+          {/* Global meta tags */}
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="description" content="Ali Permit Practice" />
+          <meta name="author" content="Jordon Marchesano" />
+          <title>Ali Permit Practice</title>
+
+          {/* Add any other global tags like fonts or external scripts */}
+        </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
